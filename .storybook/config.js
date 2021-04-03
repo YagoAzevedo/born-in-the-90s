d4f771c480a7abe35aea4911d47da79c0e905a4c
Yago Azevedo <yago.dev@gmail.com>
2021-04-03 10:47:01 -0300

    First commit

---
+++ .storybook/config.js
@@ -0,0 +1,9 @@
+import { configure, addParameters, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import centered from '@storybook/addon-centered/react';
+
+addDecorator(withInfo);
+addDecorator(centered);
+
+// Import all stories
+configure(require.context('../stories', true, /\.stories\.tsx$/), module);
